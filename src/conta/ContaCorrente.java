b46739dcd7ce7046c58a97d3a901c73e44ef4191
Eduardo <eduardoandradebomfimjr@gmail.com>
2023-10-08 16:55:52 -0300

    Finished class ContaCorrente

--- src/conta/ContaCorrente.java
+++ src/conta/ContaCorrente.java
@@ -1,16 +1,20 @@
 package conta;
 
+import exception.ECampoInvalidoException;
+import exception.ESaldoInsuficienteException;
+import exception.EValorInvalidoException;
+
 public class ContaCorrente {
 
     private int agencia;
-    private int numero;
+    private int numeroConta;
     protected float saldo;
     private String cpf;
     private String nome;
 
-    public ContaCorrente(int agencia, int numero, String nome, String cpf){
+    public ContaCorrente(int agencia, int numeroConta, String nome, String cpf){
         this.agencia = agencia;
-        this.numero = numero;
+        this.numeroConta = numeroConta;
         this.nome = nome;
         this.cpf = cpf;
         saldo = 0;
@@ -20,22 +24,35 @@ public class ContaCorrente {
         return agencia;
     }
 
-    public int getNumero() {
-        return numero;
+    public int getNumeroConta() {
+        return numeroConta;
     }
 
     public void alterarDados(String nome, String cpf){
-        this.nome = nome;
-        this.cpf = cpf;
-    }
-
-    public void saque(float valor){
-        if (valor <= saldo){
-            saldo -= valor;
+        if (cpf.length()!=12){
+            throw new ECampoInvalidoException("CPF inválido.");
+        } else if (nome.matches("^[a-zA-Z\\s']+")) {
+            this.nome = nome;
+            this.cpf = cpf;
         }
     }
 
-
+    public void saque(float valor) throws ESaldoInsuficienteException{
+        if (valor <= 0){
+            throw new EValorInvalidoException("O valor do saque deve ser positivo.");
+        }
+        if (valor <= saldo){
+            saldo -= valor;
+        }else {
+            throw new ESaldoInsuficienteException("Saldo insuficiente para efetuar o saque.");
+        }
+    }
+    public void deposito(float valor){
+        if (valor <= 0){
+            throw new EValorInvalidoException("O valor do saque deve ser positivo.");
+        }
+        saldo += valor;
+    }
 
 
 }

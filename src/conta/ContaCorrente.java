888cae33ba7ed3ab8625d0aedc1840c35c01589b
Eduardo <eduardoandradebomfimjr@gmail.com>
2023-10-07 22:05:30 -0300

    creation of the current account class

---
+++ src/conta/ContaCorrente.java
@@ -0,0 +1,14 @@
+package conta;
+
+public class ContaCorrente {
+
+    private int agencia;
+    private String numero;
+    protected float saldo;
+    private String cpf;
+    private String nome;
+
+    public ContaCorrente(String agencia, String numero, float saldo, String cpf, String nome){
+
+    }
+}

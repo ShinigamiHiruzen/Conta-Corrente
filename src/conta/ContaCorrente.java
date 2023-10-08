09a5693a506a9062b792a43c32dd3a30a55b24af
Eduardo <eduardoandradebomfimjr@gmail.com>
2023-10-07 22:39:05 -0300

    I created the methods for the class

--- src/conta/ContaCorrente.java
+++ src/conta/ContaCorrente.java
@@ -3,12 +3,39 @@ package conta;
 public class ContaCorrente {
 
     private int agencia;
-    private String numero;
+    private int numero;
     protected float saldo;
     private String cpf;
     private String nome;
 
-    public ContaCorrente(String agencia, String numero, float saldo, String cpf, String nome){
-
+    public ContaCorrente(int agencia, int numero, String nome, String cpf){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.nome = nome;
+        this.cpf = cpf;
+        saldo = 0;
     }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void alterarDados(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void saque(float valor){
+        if (valor <= saldo){
+            saldo -= valor;
+        }
+    }
+
+
+
+
 }
